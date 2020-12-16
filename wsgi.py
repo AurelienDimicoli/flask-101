5255dbd35f48f6442c00e764e91fe852153f26e1
Aurelien <aurelien.dimicoli@gmail.com>
2020-12-16 11:35:32 +0100

    test cicd 2

--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,8 @@ def hello():
 
 PRODUCTS = {
     1: {'id': 1, 'name': 'Skello'},
-    2: {'id': 2, 'name': 'Socialive.tv'}
+    2: {'id': 2, 'name': 'Socialive.tv'},
+    3: {'id': 3, 'name': 'LeWagonPA'}
 }
 
 
